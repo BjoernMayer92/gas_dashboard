8101b74754dc3740d472454f10fe06900599e1be
Björn Mayer <bjoern.mayer92@gmail.com>
2022-09-22 18:01:41 +0200

    EIC listing now exports into sql database

--- src/download/download_eic_listing.py
+++ src/download/download_eic_listing.py
@@ -2,6 +2,7 @@ import sys
 import os
 import json
 from pathlib import Path
+import sqlite3
 
 root_dir = Path.cwd().parents[1]
 conf_dir = os.path.join(root_dir,"config")
@@ -16,6 +17,12 @@ sys.path.append(str(config.SRC_DIR))
 
 import agsi_api as agsi
 
+sql_database_file = config.SQL_DATABASE_FILE
+conn = sqlite3.connect(sql_database_file)
+conn_cursor = conn.cursor()
+
+
+
 if __name__ == '__main__':
     query_string_eic_listing = "https://agsi.gie.eu/api/about"
 
@@ -25,7 +32,11 @@ if __name__ == '__main__':
     with open(file_eic_listing , 'w') as file:
         json.dump(data_eic_listing, file)
 
-    facility_query_parameter_list = agsi.generate_query_string_list(data_eic_listing, api_string=config.AGSI_API_STRING)
+    facility_query_parameter_df = agsi.generate_query_string_list(data_eic_listing, api_string=config.AGSI_API_STRING)
     
-    fille_facility_queries = os.path.join(config.DATA_AGSI_DIR,"Query_listing.json")
-    facility_query_parameter_list.to_json(fille_facility_queries)
+    file_facility_queries = os.path.join(config.DATA_AGSI_DIR,"Query_listing.json")
+    
+    #facility_query_parameter_df.to_json(file_facility_queries)
+    facility_query_parameter_df.to_sql("eic_listing",con=conn)
+
+
