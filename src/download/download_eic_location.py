4d45a126b7308251d5a220cfe4055615d0b07e84
Björn Mayer <bjoern.mayer92@gmail.com>
2022-09-22 19:30:47 +0200

    location is now written to sql database

--- src/download/download_eic_location.py
+++ src/download/download_eic_location.py
@@ -4,6 +4,7 @@ import os
 import requests
 from pathlib import Path
 import pandas as pd
+import sqlite3
 
 
 
@@ -23,6 +24,10 @@ sys.path.append(str(config.SRC_DIR))
 
 import agsi_api as agsi
 
+sql_database_file = config.SQL_DATABASE_FILE
+conn = sqlite3.connect(sql_database_file)
+conn_cursor = conn.cursor()
+
 if __name__=='__main__':
     file_facility_queries = os.path.join(config.DATA_AGSI_DIR,"Query_listing.json")
 
@@ -32,7 +37,20 @@ if __name__=='__main__':
     location_coordinates_lat_list = []
     location_coordinates_lon_list = []
 
-    for name in data["name"]:    
+
+    table_info = conn_cursor.execute("PRAGMA TABLE_INFO(eic_listing)").fetchall()
+    table_cols = list(map( lambda x: x[1], table_info))
+
+    if not("location_name" in table_cols):
+        conn_cursor.execute("alter table eic_listing add column '{}' 'str'".format("location_name"))
+    if not ("lat" in table_cols):
+        conn_cursor.execute("alter table eic_listing add column '{}' 'float'".format("lat"))
+    if not ("lon" in table_cols):
+        conn_cursor.execute("alter table eic_listing add column '{}' 'float'".format("lon"))
+    
+    for name in conn_cursor.execute('SELECT name FROM eic_listing') : 
+        name = name[0]
+        print(name)
         query_string_parameter_dict = {"input":name, "key":keys.GOOGLE}
 
         query_string = agsi.generate_query_string(config.GOOGLE_API_STRING, query_string_parameter_dict = query_string_parameter_dict)
@@ -46,15 +64,15 @@ if __name__=='__main__':
             location_name = resp_json["results"][0]["name"]
         else: 
             location_name  = "NA"
-            location_coordinates = {"lat":"NA","lng":"NA"}
-        
+            location_coordinates = {"lat":"NA","lng":"NA"}  
 
-        location_name_list.append(location_name)
-        location_coordinates_lat_list.append(location_coordinates["lat"])
-        location_coordinates_lon_list.append(location_coordinates["lng"])
-    
-    data["location_name"] = location_name_list
-    data["lat"] = location_coordinates_lat_list 
-    data["lon"] = location_coordinates_lon_list 
-    
-    data.to_json(file_facility_queries)
+        conn_data_cursor = conn.cursor()
+        sql = ''' UPDATE eic_listing
+              SET lat = ? ,
+                  lon = ? ,
+                  location_name = ?
+              WHERE name = ?'''
+
+        conn_data_cursor.execute(sql,[location_coordinates["lat"],location_coordinates["lng"], location_name, name])
+        conn.commit()
+       
