838088374a36231d759bf78706877fdaffd59bd2
Bjoern Mayer <bjoern.mayer92@gmail.com>
2023-04-24 23:16:28 +0200

    Added Documenation

--- src/agsi_api.py
+++ src/agsi_api.py
@@ -6,6 +6,10 @@ import numpy as np
 from pathlib import Path
 from tqdm import tqdm
 from sqlite_queries import *
+import logging
+logging.basicConfig(level=logging.INFO)
+
+
 
 root_dir = Path.cwd().parents[0]
 conf_dir = os.path.join(root_dir,"config")
@@ -14,15 +18,14 @@ sys.path.append(str(conf_dir))
 
 import keys
 
-def generate_query_string(api_string, query_string_parameter_dict):
-    """_summary_
-
+def generate_query_string(api_string: str, query_string_parameter_dict: dict)-> str:
+    """ This function generates a query string for a given dictionary of queries and an api string.
     Args:
-        api_string (_type_): _description_
-        query_string_parameter_dict (_type_): _description_
+        api_string (str): Base string of the query
+        query_string_parameter_dict (str): dictionary of query parameters
 
     Returns:
-        _type_: _description_
+        str: Generated query string
     """
     query_string = api_string
     for query_string_parameter_name in query_string_parameter_dict:
@@ -32,14 +35,33 @@ def generate_query_string(api_string, query_string_parameter_dict):
 
     return query_string
 
-def decompose_query_string(query_string):
-    """_summary_
+
+def request_query_as_json(query_string: str, api_key:str=keys.AGSI)->dict:
+    """This function takes a query string and the api key to perform the actual request and returns the result as a json dictionary.
 
     Args:
-        query_string (_type_): _description_
+        query_string (str): Full query string
+        api_key (str, optional): API key. Defaults to keys.AGSI:str.
 
     Returns:
-        _type_: _description_
+        dict: JSON formated result of the query
+    """
+    header = {}
+    header["x-key"] = keys.AGSI
+    
+    resp = requests.get(query_string, headers=header)
+    data = resp.json()
+
+    return data
+
+def decompose_query_string(query_string:str) -> dict:
+    """ THis function takes an api query string and returns a dictionary containing the properties of the query
+
+    Args:
+        query_string (str): Query string to be decomposed.
+
+    Returns:
+        dict: Dictionary containing the parameters of the query.
     """
     query_string_parameter_string_list = query_string.split("&")[1:]
     query_string_parameter_dict = {}
@@ -52,7 +74,7 @@ def decompose_query_string(query_string):
 
 
 
-def query_eic_listing(conn, data_eic_listing, country_name="Germany"):
+def query_companies_and_facilities_from_eic_listing(conn, data_eic_listing, company_type="SSO", type="Europe", country_name="Germany"):
     """_summary_
 
     Args:
@@ -61,16 +83,10 @@ def query_eic_listing(conn, data_eic_listing, country_name="Germany"):
     Returns:
         _type_: _description_
     """
+    logging.info("query_eic_listing")
 
-    table_cursor = conn.cursor()
-    
-    table_cursor.execute(create_company_table_query)
-    table_cursor.execute(create_facility_table_query)
-
-
-    
-    company_list = data_eic_listing["SSO"]["Europe"][country_name]
-    
+    company_list = data_eic_listing[company_type][type][country_name]
+    logging.info("company_list {}".format(company_list))
     
     company_cursor = conn.cursor()
     
@@ -78,13 +94,12 @@ def query_eic_listing(conn, data_eic_listing, country_name="Germany"):
         company_eic = company["eic"]
         company_name = company["name"]
         company_short_name = company["short_name"]
-        company_url = company["url"]
         company_country = company["data"]["country"]["code"]
         company_type = company["data"]["type"]
         company_image = company["image"]
 
-        company_cursor.execute(insert_company_query, (company_eic, company_name,company_short_name, company_url, company_type, company_country, company_image))
-        conn.commit()    
+        company_cursor.execute(insert_company_query, (company_eic, company_name,company_short_name, company_type, company_country, company_image))
+        conn.commit()
         
         facility_list = company["facilities"]
         facility_cursor = conn.cursor()
@@ -139,24 +154,6 @@ def query_facility_locations(facilities_df, conn, api_string, manual_data):
     conn.commit()
 
 
-def request_query_as_json(query_string, api_key=keys.AGSI):
-    """_summary_
-
-    Args:
-        query_string (_type_): _description_
-        api_key (_type_, optional): _description_. Defaults to keys.AGSI.
-
-    Returns:
-        _type_: _description_
-    """
-    header = {}
-    header["x-key"] = keys.AGSI
-    
-    resp = requests.get(query_string, headers=header)
-    data = resp.json()
-
-    return data
-
 
 
 
@@ -174,12 +171,12 @@ def query_facility_historical_timeseries(facilities_df,conn, agsi_api_string, ag
 
         query_dict = {"country":country, "company": company_eic, "facility":facility_eic}
         query_string = generate_query_string(agsi_api_string,query_dict)
-        
+        logging.info(query_string+"\n")
         initial_request_data = request_query_as_json(query_string)
         number_of_pages = initial_request_data["last_page"]
 
+        
         insert_cursor = conn.cursor()
-
 
         for page in tqdm(range(1,number_of_pages+1), leave=False):
             query_page_dict = {"country":country, "company": company_eic, "facility":facility_eic, "page":str(page)}
